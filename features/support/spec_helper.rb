271571c1853128b556a324c69d91146b3a4c1ca8
Camila <nascisou@inmetrics.com>
2017-04-12 10:16:00 -0300

    Remoçao dos arquivos lixo

--- features/support/spec_helper.rb
+++ features/support/spec_helper.rb
@@ -3,19 +3,4 @@
 
 Dir[File.join(File.dirname(__FILE__), '../pages/*.rb')].each { |file| require file }
 
-module UI
-  module Pages
-    def mercury_page
-      UI::Pages::MercuryPage.new
-    end
-    def fly_page
-      UI::Pages::FlyPage.new
-    end
-    def DataFlyPage_page
-      UI::Pages::DataFlyPage.new
-    end
-    def BookFlyPage_page
-      UI::Pages::BookFlyPage.new
-    end
-  end
-end
+
